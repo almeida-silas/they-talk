36a522580e4183fc1db47d2affbedb0a1f35f198
Silas Almeida <silasadssn@gmail.com>
2020-04-12 03:10:55 -0300

    server

---
+++ server/src/server.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const port = 3333;
+
+const server = require('socket.io')(http, {
+   serveClient: false,
+   pingInterval: 10000,
+   pingTimeout: 5000,
+   cookie: false,
+});
+
+server.listen(port, () => {
+   console.log(`server running in port ${port}`);
+});
+
+//
+let messages = []
+
+server.on('connection', socket => {
+
+   socket.emit('chat.messages', messages);
+   server.emit('clients', server.engine.clientsCount);
+
+   socket.on('send.message', data => {
+      messages.push(data);
+      server.emit('received.messages', messages);
+   });
+
+   socket.on('disconnect', () => {
+      socket.broadcast.emit('disconnected', socket.id);
+      server.emit('clients', server.engine.clientsCount);
+   });
+});
